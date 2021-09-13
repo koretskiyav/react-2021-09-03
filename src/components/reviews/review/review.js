9aa332394d608628d53b8c3475cb693d0c352202
Shukshina.MI <shukshina.MI@parma.ru>
2021-09-13 17:54:36 +0500

    added propTypes for all components

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,5 +1,7 @@
 import Rate from '../../rate';
 import styles from './review.module.css';
+import PropTypes from 'prop-types';
+import Reviews from '../reviews';
 
 const Review = ({ user, text, rating }) => (
   <div className={styles.review}>
@@ -17,6 +19,12 @@ const Review = ({ user, text, rating }) => (
 
 Review.defaultProps = {
   user: 'Anonymous',
+};
+
+Review.propTypes = {
+  user: PropTypes.string,
+  text: PropTypes.string,
+  rating: PropTypes.number.isRequired,
 };
 
 export default Review;
