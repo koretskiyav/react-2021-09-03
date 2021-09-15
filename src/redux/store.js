f4b51ae7cb35d979d6f4a02cec2853f5bbb38498
Detrimon <detrimon@gmail.com>
2021-09-15 17:55:52 +0300

    Prepare before HT3

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,4 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
 
-<<<<<<< HEAD
-export default createStore(reducer);  
-=======
 export default createStore(reducer);
->>>>>>> 6a6ece76ab0d804613281ec6820a1be74a053129
