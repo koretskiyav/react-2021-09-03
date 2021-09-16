1fe1c866d432eb893a5dcca402dd3d08550583fa
Artem Danilov <danilov.arrtem@gmail.com>
2021-09-16 11:46:18 +0300

    add routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,18 +2,27 @@ import { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Restaurants from '../restaurants';
 import Header from '../header';
+import { Route, Switch } from 'react-router-dom';
+import { Basket } from '../basket';
 
 export default class App extends PureComponent {
   render() {
     return (
       <div>
         <Header />
-        <Restaurants restaurants={this.props.restaurants} />
+        <Switch>
+          <Route path='/' exact>
+            <Restaurants restaurants={this.props.restaurants} />
+          </Route>
+          <Route path='/basket' exact>
+            <Basket />
+          </Route>
+        </Switch>
       </div>
     );
   }
 }
 
 App.propTypes = {
-  restaurants: PropTypes.array,
+  restaurants: PropTypes.array
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import { Provider } from 'react-redux';
 import './index.css';
+import { createBrowserHistory } from 'history';
 
 import App from './components/app';
 import ReactDOM from 'react-dom';
 import { restaurants } from './fixtures';
 import store from './redux/store';
+import { Router } from 'react-router-dom';
 
 // DEV ONLY!!!
 window.store = store;
 
+export const history = createBrowserHistory();
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App restaurants={restaurants} />
-  </Provider>,
+  <Router history={history}>
+    <Provider store={store}>
+      <App restaurants={restaurants} />
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
